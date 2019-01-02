eef26958b80ed8649dc0a7d26f32e76d74568e40
Hosung Lim <holnet1026@gmail.com>
2019-01-03 03:02:57 +0900

    feat: add calendar range example

--- examples/ExamplePage.tsx
+++ examples/ExamplePage.tsx
@@ -1,13 +1,64 @@
 import * as React from 'react';
 import RangeSelect from './RangeSelect';
-import DatePicker from '../src/DatePicker';
+import Calendar from '../src/Calendar';
 
-class ExamplePage extends React.Component<{}, {}> {
+import '../assets/styles/calendar.scss';
+import * as moment from 'moment';
+
+interface State {
+  startDay?: moment.Moment
+  endDay?: moment.Moment
+}
+class ExamplePage extends React.Component<{}, State> {
+
+  state ={
+    startDay: null,
+    endDay: null
+  }
+
+  handleChange = (date) => {
+    const { startDay, endDay } = this.state;
+    if(!startDay) {
+      this.setState({
+        startDay: date
+      });
+    } else {
+      if(date.isAfter(startDay)) {
+        this.setState({
+          endDay: date
+        });
+      } else {
+        this.setState({
+          startDay: date
+        });
+      }
+    }
+  }
+
+  getDayText = (date) => {
+    const value = date.format('YYYYMMDD');
+    const { startDay, endDay } = this.state;
+    
+    const textMap = {
+      "20190101": "신정"
+    };
+    if(startDay) textMap[startDay.format('YYYYMMDD')] = "출발일"
+    if(endDay) textMap[endDay.format('YYYYMMDD')] = "도착일"
+    return textMap[value];
+  }
 
   render() {
+    const { startDay, endDay } = this.state;
     return (
-      <DatePicker/>
-    )
+      <div className="App">
+        <Calendar
+          onChange={this.handleChange}
+          startDay={startDay}
+          endDay={endDay}
+          customDayText={this.getDayText}
+        />
+      </div>
+    );
   }
 }
 
