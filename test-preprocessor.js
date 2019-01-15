4d0a3d1a8e17e5418bd1243a5b532cfcc9837db8
Hosung Lim <holnet1026@gmail.com>
2019-01-15 23:25:22 +0900

    feat: add jest typescript configuration (#5)

---
+++ test-preprocessor.js
@@ -0,0 +1,15 @@
+/**
+ * Test Code Preprocessing 
+ * Typescript code transplie to javascript 
+ */
+const tsc = require('typescript');
+const tsConfig = require('./tsconfig.json');
+
+module.exports = {
+  process(src, path) {
+      if (path.endsWith('.ts') || path.endsWith('.tsx') || path.endsWith('.js')) {
+          return tsc.transpile(src, tsConfig.compilerOptions, path, []);
+      }
+      return src;
+  },
+};
