a87e9f39f9d117eccac01c5f79e7eb495d38f360
Hosung Lim <holnet1026@gmail.com>
2019-01-20 03:55:38 +0900

    feat: add getYear, Month matrix func

--- src/util/DateUtil.ts
+++ src/util/DateUtil.ts
@@ -23,11 +23,22 @@ export const getDayMatrix = (year?: number, month?: number): string[][] => {
   );
 };
 
-export const isDayEqual = (day1: moment.Moment, day2: moment.Moment) => {
+export const getMonthMatrix = () => {
+  const months = moment.monthsShort();
+  return chunk(months, 3);
+};
+
+export const getYearMatrix = (year: number) => {
+  return chunk(range(year - 4, year + 5).map(v => `${v}`), 3);
+};
+
+export const isDayEqual = (day1?: moment.Moment, day2?: moment.Moment) => {
+  if (!day1 || !day2) return false;
   return day1.format('YYYYMMDD') === day2.format('YYYYMMDD');
 };
 
-export const isDayRange = (date: moment.Moment, start: moment.Moment, end: moment.Moment) => {
+export const isDayRange = (date: moment.Moment, start?: moment.Moment, end?: moment.Moment) => {
+  if (!start || !end) return false;
   const updateDate = date
     .hour(0)
     .minute(0)
