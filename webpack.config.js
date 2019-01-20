6a5d1e04a2842808575c7b07d51be177c362e754
Hosung Lim <holnet1026@gmail.com>
2019-01-21 02:32:50 +0900

    refactor: change babel-loader to ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // Is the current build a development build
-const IS_DEV = (process.env.NODE_ENV === 'dev');
+const IS_DEV = process.env.NODE_ENV === 'dev';
 
 const dirNode = 'node_modules';
 const dirSrc = path.join(__dirname, 'src');
@@ -16,84 +16,79 @@ const appHtmlTitle = 'DatePicker Examples';
  * Webpack Configuration
  */
 module.exports = {
-	entry: {
-		vendor: [
-			'lodash'
-		],
-		bundle: path.join(dirExamples, 'index.tsx')
-	},
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js', '.jsx']
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			IS_DEV: IS_DEV
-		}),
+  entry: {
+    vendor: ['lodash'],
+    bundle: path.join(dirExamples, 'index.tsx'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEV: IS_DEV,
+    }),
 
-		new HtmlWebpackPlugin({
-			template: path.join(__dirname, 'examples/index.ejs'),
-			title: appHtmlTitle
-		})
-	],
-	module: {
-		rules: [
-			// BABEL
-			{
-				test: /\.js$|.tsx$|.ts$|.jsx$/,
-				loader: 'babel-loader',
-				exclude: /(node_modules)/,
-				options: {
-					compact: true
-				}
-			},
-
-			// STYLES
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					{
-						loader: 'css-loader',
-						options: {
-							sourceMap: IS_DEV
-						}
-					},
-				]
-			},
-
-			// CSS / SASS
-			{
-				test: /\.scss/,
-				use: [
-					'style-loader',
-					{
-						loader: 'css-loader',
-						options: {
-							sourceMap: IS_DEV
-						}
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							sourceMap: IS_DEV,
-							includePaths: [dirAssets]
-						}
-					}
-				]
-			},
-			{
-        loader: "url-loader",
-        test: /\.(svg|eot|ttf|woff|woff2)?$/
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'examples/index.ejs'),
+      title: appHtmlTitle,
+    }),
+  ],
+  module: {
+    rules: [
+      // BABEL
+      {
+        test: /\.ts$|.tsx$/,
+        loader: 'ts-loader',
+        exclude: /(node_modules)/,
       },
 
-			// IMAGES
-			{
-				test: /\.(jpe?g|png|gif)$/,
-				loader: 'file-loader',
-				options: {
-					name: '[path][name].[ext]'
-				}
-			}
-		]
-	}
+      // STYLES
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: IS_DEV,
+            },
+          },
+        ],
+      },
+
+      // CSS / SASS
+      {
+        test: /\.scss/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: IS_DEV,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: IS_DEV,
+              includePaths: [dirAssets],
+            },
+          },
+        ],
+      },
+      {
+        loader: 'url-loader',
+        test: /\.(svg|eot|ttf|woff|woff2)?$/,
+      },
+
+      // IMAGES
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+    ],
+  },
 };
