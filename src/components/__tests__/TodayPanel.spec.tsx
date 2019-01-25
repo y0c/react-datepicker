35850d3d1f7d1b2513d073d0b81a4c7567415295
Hosung Lim <holnet1026@gmail.com>
2019-01-25 22:06:37 +0900

    test: add today panel comp test case

---
+++ src/components/__tests__/TodayPanel.spec.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import * as sinon from 'sinon';
+import TodayPanel from '../TodayPanel';
+import { shallow, ShallowWrapper } from 'enzyme';
+
+describe('<TodayPanel/>', () => {
+  let component: ShallowWrapper<React.Component>;
+  let onClick: sinon.SinonSpy;
+
+  beforeEach(() => {
+    onClick = sinon.spy();
+    component = shallow(<TodayPanel onClick={onClick} today="20190101" show />);
+  });
+
+  it('renders without props', () => {
+    expect(component).toBeTruthy();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('props show correctly', () => {
+    expect(component.hasClass('calendar__panel--show')).toBeTruthy();
+  });
+
+  it('props onClick correctly', () => {
+    component.find('h2').simulate('click');
+    expect(onClick).toHaveProperty('callCount', 1);
+  });
+
+  it('props today correctly', () => {
+    expect(component.find('h2').text()).toEqual('20190101');
+  });
+});
