ae15f5b4c8ff8233b9effbd3184500b1ce3129ff
Hosung Lim <holnet1026@gmail.com>
2019-01-30 23:44:10 +0900

    feat: add storyboard addon knobs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,8 @@
 import { addDecorator, configure } from '@storybook/react';
 import { withTests } from '@storybook/addon-jest';
 import results from '../.jest.test.result.json';
-
+import { withKnobs } from '@storybook/addon-knobs';
+import 'assets/styles/calendar.scss';
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.tsx$/);
 function loadStories() {
@@ -14,5 +15,7 @@ addDecorator(
     filesExt: '.test.tsx',
   })
 );
+
+addDecorator(withKnobs);
 
 configure(loadStories, module);
