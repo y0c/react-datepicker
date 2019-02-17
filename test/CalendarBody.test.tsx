bab7a2a3fce3702f27473b4a44ee4f1ebdccc6b9
Hosung Lim <holnet1026@gmail.com>
2019-02-17 17:22:39 +0900

    test: update calendarbody snapshot

--- test/CalendarBody.test.tsx
+++ test/CalendarBody.test.tsx
@@ -1,21 +1,23 @@
 import { mount, shallow, ReactWrapper, ShallowWrapper } from 'enzyme';
+import * as sinon from 'sinon';
 import * as moment from 'moment';
 import * as React from 'react';
-import * as sinon from 'sinon';
 import DayView from '../src/components/DayView';
-import TestCalendarBody from '../src/components/CalendarBody';
+import CalendarBody from '../src/components/CalendarBody';
 import { IDatePicker } from '../src/common/@types';
 
 describe('<CalendarBody />', () => {
   // 20181201
   const mockMoment = moment.unix(1543622400);
+  const defaultProps = {
+    current: mockMoment,
+    onClick: sinon.spy(),
+  };
   let shallowComponent: ShallowWrapper<React.Component>;
   let mountComponent: ReactWrapper;
 
-  const CalendarBody = (props: any) => <TestCalendarBody current={mockMoment} {...props} />;
-
   it('should render correctly', () => {
-    shallowComponent = shallow(<CalendarBody />);
+    shallowComponent = shallow(<CalendarBody {...defaultProps} />);
     expect(shallowComponent).toBeTruthy();
     expect(shallowComponent).toMatchSnapshot();
   });
@@ -23,19 +25,22 @@ describe('<CalendarBody />', () => {
   describe('prop: viewMode', () => {
     it('should ViewMode.DAY correctly', () => {
       shallowComponent = shallow(
-        <TestCalendarBody current={mockMoment} viewMode={IDatePicker.ViewMode.DAY} />
+        <CalendarBody {...defaultProps} viewMode={IDatePicker.ViewMode.DAY} />
       );
-      expect(shallowComponent).toMatchSnapshot();
       expect(shallowComponent.find(DayView)).toHaveLength(1);
     });
 
     it('should ViewMode.MONTH correctly', () => {
-      mountComponent = mount(<CalendarBody viewMode={IDatePicker.ViewMode.MONTH} />);
+      mountComponent = mount(
+        <CalendarBody {...defaultProps} viewMode={IDatePicker.ViewMode.MONTH} />
+      );
       expect(mountComponent.find('td.calendar__month')).toHaveLength(12);
     });
 
     it('should ViewMode.YEAR correctly', () => {
-      mountComponent = mount(<CalendarBody viewMode={IDatePicker.ViewMode.YEAR} />);
+      mountComponent = mount(
+        <CalendarBody {...defaultProps} viewMode={IDatePicker.ViewMode.YEAR} />
+      );
       expect(mountComponent.find('td.calendar__year')).toHaveLength(9);
     });
   });
