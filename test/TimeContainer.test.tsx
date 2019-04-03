8542857c5a797f141423f74f7a164ce85335b8d1
Hosung Lim <holnet1026@gmail.com>
2019-04-03 21:39:46 +0900

    test: 👷‍♂️ add missing test case

--- test/TimeContainer.test.tsx
+++ test/TimeContainer.test.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import * as sinon from 'sinon';
-import { range } from 'lodash';
 import { shallow, mount, ReactWrapper } from 'enzyme';
+import { mountInputSimulateChange } from './utils/TestingUtil';
 import TimeContainer from '../src/components/TimeContainer';
 
 describe('<TimeContainer/>', () => {
   let mountComponent: ReactWrapper;
   let onChange: sinon.SinonSpy;
+  let onBlur: sinon.SinonSpy;
 
   const btnClick = (type: string, at: number) => {
     mountComponent
@@ -15,61 +16,80 @@ describe('<TimeContainer/>', () => {
       .simulate('click');
   };
 
-  const radioChange = (at: number) => {
-    mountComponent
-      .find('.radio')
-      .at(at)
-      .simulate('change');
-  };
-
   beforeEach(() => {
     onChange = sinon.spy();
-    mountComponent = mount(<TimeContainer onChange={onChange} />);
+    onBlur = sinon.spy();
+    mountComponent = mount(<TimeContainer onChange={onChange} onBlur={onBlur} />);
   });
 
-  it('should hour up correctly', () => {
+  it('should hour not be exceeded maximum value', () => {
+    mountComponent.setState({
+      ...mountComponent.state,
+      hour: 23,
+    });
     btnClick('up', 0);
-    expect(mountComponent.state('hour')).toEqual(2);
+    expect(mountComponent.state('hour')).toBe(23);
+  });
 
-    range(11).forEach(() => btnClick('up', 0));
-    expect(mountComponent.state('hour')).toEqual(1);
-
+  it('should hour not be below minimum value', () => {
+    mountComponent.setState({
+      ...mountComponent.state,
+      hour: 0,
+    });
     btnClick('down', 0);
-    expect(mountComponent.state('hour')).toEqual(12);
-
-    // if min -1 -> max value
-    range(12).forEach(() => btnClick('down', 0));
-    expect(mountComponent.state('hour')).toEqual(12);
+    expect(mountComponent.state('hour')).toBe(0);
   });
 
-  it('should minute up & down correctly', () => {
+  it('should minute not be exceeded maximum value', () => {
+    mountComponent.setState({
+      ...mountComponent.state,
+      minute: 59,
+    });
     btnClick('up', 1);
-    expect(mountComponent.state('minute')).toEqual(1);
-
-    btnClick('down', 1);
-    expect(mountComponent.state('minute')).toEqual(0);
-
-    // if min -1 -> max value
-    btnClick('down', 1);
-    expect(mountComponent.state('minute')).toEqual(59);
+    expect(mountComponent.state('minute')).toBe(59);
   });
 
-  it('should timetype change correctly', () => {
-    radioChange(0);
-    expect(mountComponent.state('type')).toEqual('AM');
-
-    radioChange(1);
-    expect(mountComponent.state('type')).toEqual('PM');
+  it('should minute not be below minimum value', () => {
+    mountComponent.setState({
+      ...mountComponent.state,
+      minute: 0,
+    });
+    btnClick('down', 0);
+    expect(mountComponent.state('minute')).toBe(0);
   });
 
-  it('should onChange called correctly', () => {
-    btnClick('up', 0);
-    btnClick('up', 1);
-    radioChange(0);
+  it('should input change call onChnage func', () => {
     mountComponent
       .find('.time-input__text input')
       .at(0)
       .simulate('change');
-    expect(onChange).toHaveProperty('callCount', 4);
+
+    expect(onChange).toHaveProperty('callCount', 1);
+  });
+
+  it('should input blur call onBlur func', () => {
+    mountComponent
+      .find('.time-input__text input')
+      .at(0)
+      .simulate('blur');
+
+    expect(onBlur).toHaveProperty('callCount', 1);
+  });
+
+  describe('handleChange', () => {
+    it('should input non number set value 0', () => {
+      mountInputSimulateChange(mountComponent.find('.time-input__text input').at(0), 'test');
+      expect(mountComponent.state('hour')).toBe(0);
+    });
+
+    it('should input value exceeded maximum value set value max value', () => {
+      mountInputSimulateChange(mountComponent.find('.time-input__text input').at(0), '40');
+      expect(mountComponent.state('hour')).toBe(23);
+    });
+
+    it('should input value below minimum value set value min value', () => {
+      mountInputSimulateChange(mountComponent.find('.time-input__text input').at(0), '-1');
+      expect(mountComponent.state('hour')).toBe(0);
+    });
   });
 });
